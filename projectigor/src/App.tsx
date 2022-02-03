bc0ade8ffc9b578a9084572afba64ad5c9c89b47
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-02-03 12:19:52 +0300

    added new project

---
+++ projectigor/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+    <button onClick={(event) => {
+      console.log("hello!")}}>MyYouTubeChannel-1</button>
+    </div>
+  );
+}
+
+export default App;
