ff86c64f39b5bf6caaf940254e986de33313aa64
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-02-03 14:50:02 +0300

    change files

--- projectigor/src/App.tsx
+++ projectigor/src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import './App.css';
-
+import {Button} from "./Button/components/Button";
 
 
 function App() {
+    const Button1Foo = (subscriber:string, age: number, address: string) => {
+        console.log(subscriber + ", I am " + age + " years old. " + address)
+    }
+    const Button2Foo = (subscriber:string, age: number) => {
+        console.log(subscriber + ", I am " + age + " years old.")
+    }
+    const Button3Foo = () => {
+        console.log("I am Stupid Button")
+    }
 
-  return (
-    <div className="App">
-    <button onClick={(event) => {
-      console.log("hello!")}}>MyYouTubeChannel-1</button>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            {/*<button>MyYouTubeChannel-1</button>
+            <button>MyYouTubeChannel-2</button>*/}
+            <Button name={"MyYouTubeChannel-1"} callback={() => Button1Foo("I am Andrey", 31, "I live in Moscow")}/>
+            <Button name={"MyYouTubeChannel-2"} callback={() => Button2Foo("I am Olga", 33)}/>
+            <Button name={"Button"} callback={Button3Foo}/>
+
+        </div>
+    );
 }
 
 export default App;
