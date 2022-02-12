6f8c40a260534d6c937366548d9761e58aa8dd9c
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-02-12 11:42:57 +0300

    change files

--- projectigor/src/Map/components/NewComponent.tsx
+++ projectigor/src/Map/components/NewComponent.tsx
@@ -14,18 +14,42 @@ type StudentType = {
 
 
 const NewComponent = (props: NewComponentPropsType) => {
+
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
+
     return (
-        <ul>
+        /*<ul>
             {props.students.map((objectFromStudentArray, index, array) => {
                 return (
                     <li key={objectFromStudentArray.id}>
-                    <span>{objectFromStudentArray.name}</span>
-                    <span> age: {objectFromStudentArray.age}</span>
-
+                        <span>{objectFromStudentArray.name}</span>
+                        <span> age: {objectFromStudentArray.age}</span>
                     </li>
                 )
             })}
-        </ul>
+        </ul>*/
+        <div>
+            {topCars.map((carsObject, index) => {
+                return (
+                    <table>
+                        <tr>
+                            <th>№</th>
+                            <th>Manufacturer</th>
+                            <th>Model</th>
+                        </tr>
+                        <tr>
+                            <td>{index + 1}</td>
+                            <td>{carsObject.manufacturer}</td>
+                            <td>{carsObject.model}</td>
+                        </tr>
+                    </table>
+                )
+            })}
+        </div>
     );
 };
 
