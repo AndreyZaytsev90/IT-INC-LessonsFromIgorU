96c7095fc0225a195cf7ee38ad8d6612b05c1a7b
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-02-03 21:50:56 +0300

    Update file

--- projectigor/src/App2023.tsx
+++ projectigor/src/App2023.tsx
@@ -16,139 +16,140 @@ type FilterType = 'All' | 'Rubles' | 'Dollars'
 
 const App2023 = () => {
 
-  const students = [
-    {id: 1, name: "James", age: 8},
-    {id: 2, name: "Robert", age: 18},
-    {id: 3, name: "John", age: 28},
-    {id: 4, name: "Michael", age: 38},
-    {id: 5, name: "William", age: 48},
-    {id: 6, name: "David", age: 58},
-    {id: 7, name: "Richard", age: 68},
-    {id: 8, name: "Joseph", age: 78},
-    {id: 9, name: "Thomas", age: 88},
-    {id: 10, name: "Charles", age: 98},
-    {id: 11, name: "Christopher", age: 100},
-  ]
+    const students = [
+        {id: 1, name: "James", age: 8},
+        {id: 2, name: "Robert", age: 18},
+        {id: 3, name: "John", age: 28},
+        {id: 4, name: "Michael", age: 38},
+        {id: 5, name: "William", age: 48},
+        {id: 6, name: "David", age: 58},
+        {id: 7, name: "Richard", age: 68},
+        {id: 8, name: "Joseph", age: 78},
+        {id: 9, name: "Thomas", age: 88},
+        {id: 10, name: "Charles", age: 98},
+        {id: 11, name: "Christopher", age: 100},
+    ]
 
-  const topCars = [
-    {manufacturer: 'BMW', model: 'm5cs'},
-    {manufacturer: 'Mercedes', model: 'e63s'},
-    {manufacturer: 'Audi', model: 'rs6'}
-  ]
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
 
-  /*  const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-        console.log("Hello i am Andrey")
+    /*  const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+          console.log("Hello i am Andrey")
+      }
+      const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+          console.log("Hello i am Olga")
+      }
+  */
+    const onClickHandler = (name: string) => {
+        console.log(`Hello i am ${name}`)
     }
-    const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-        console.log("Hello i am Olga")
+    /* const function1 = () => {
+         console.log(100200)
+     }
+     const function2 = (num: number) => {
+         console.log(num)
+     }*/
+    const ButtonOne = (subscriber: string, age: number) => {
+        console.log(`Hello i am ${subscriber}, I'm ${age} years old`)
     }
-*/
-  const onClickHandler = (name: string) => {
-    console.log(`Hello i am ${name}`)
-  }
-  /* const function1 = () => {
-       console.log(100200)
-   }
-   const function2 = (num: number) => {
-       console.log(num)
-   }*/
-  const ButtonOne = (subscriber: string, age: number) => {
-    console.log(`Hello i am ${subscriber}, I'm ${age} years old`)
-  }
-  const ButtonTwo = (subscriber: string, age: number) => {
-    console.log(`Hello i am ${subscriber}, I'm ${age} years old`)
-  }
-  const ButtonThree = () => {
-    console.log('I am a stupid button')
-  }
+    const ButtonTwo = (subscriber: string, age: number) => {
+        console.log(`Hello i am ${subscriber}, I'm ${age} years old`)
+    }
+    const ButtonThree = () => {
+        console.log('I am a stupid button')
+    }
 
 
-  //4. UseState
-  //let a = 1
-  let [a, setA] = useState(0)
+    //4. UseState
+    //let a = 1
+    let [a, setA] = useState(0)
 
-  const plusOne = () => {
-    setA(++a)
-  }
-  const zero = () => {
-    setA(0)
-  }
+    const plusOne = () => {
+        setA(++a)
+    }
+    const zero = () => {
+        setA(0)
+    }
 
-  //5. Filter
-  const [money, setMoney] = useState([
-    {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
-    {banknotes: 'Rubles', value: 100, number: ' w1234567890'},
-    {banknotes: 'Dollars', value: 100, number: ' e1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' c1234567890'},
-    {banknotes: 'Rubles', value: 100, number: ' r1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' x1234567890'},
-    {banknotes: 'Rubles', value: 50, number: ' v1234567890'},
-  ])
+    //5. Filter
+    const [money, setMoney] = useState([
+        {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknotes: 'Rubles', value: 100, number: ' w1234567890'},
+        {banknotes: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknotes: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknotes: 'Rubles', value: 100, number: ' r1234567890'},
+        {banknotes: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknotes: 'Rubles', value: 50, number: ' v1234567890'},
+    ])
 
-  const [filter, setFilter] = useState<FilterType>('All')
+    const [filter, setFilter] = useState<FilterType>('All')
 
-  let currentMoney = money
-  if (filter === 'Dollars') {
-    currentMoney = money.filter((filteredMoney) => filteredMoney.banknotes === 'Dollars')
-  }
-  if (filter === 'Rubles') {
-    currentMoney = money.filter((filteredMoney) => filteredMoney.banknotes === 'Rubles')
-  }
+    let currentMoney = money
+    if (filter === 'Dollars') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknotes === 'Dollars')
+    }
+    if (filter === 'Rubles') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknotes === 'Rubles')
+    }
 
-  const OnClickFilterHandler = (nameButton: FilterType) => {
-    setFilter(nameButton)
-  }
+    const OnClickFilterHandler = (nameButton: FilterType) => {
+        setFilter(nameButton)
+    }
 
 
-  return (
-    <>
-      <body>
-      <header>
-        <div className="container">
-          <a href="#" target='_blank'>
-            <img src={Logo} alt="logo"/>
-          </a>
-          <Header2023 title={'HelloWorld!'}/>
-          <Button2023 name={"LOGIN"} callback={() => {
-          }}/>
-        </div>
-      </header>
-      <section>
-        <h1>
-          <Body2023 text={"I am Andrey and i a programmer!"}/>
-        </h1>
-        <div className={styles.card}>
-          <h3>MAP</h3>
-          <div className={styles.content}>
-            <NewComponent2023 students={students} cars={topCars}/>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <h3>Button</h3>
-          <div className={styles.content}>
-            {/*<button onClick={(event) => {console.log("Hello")}}>MyYouTubeChannel-1</button>*/}
-            <button onClick={(event) => onClickHandler("Andrey")}>MyYouTubeChannel-1</button>
-            <button onClick={(event) => onClickHandler("Olga")}>MyYouTubeChannel-2</button>
-            {/*   <button onClick={function1}>1</button>
+    return (
+        <>
+            <body>
+            <header>
+                <div className={styles.container}>
+                    <a href="#" target='_blank'>
+                        <img src={Logo} alt="logo"/>
+                    </a>
+                    <Header2023 title={'HelloWorld!'}/>
+                    <Button2023 name={"LOGIN"} callback={() => {
+                    }}/>
+                </div>
+            </header>
+            <section>
+                <div className={styles.container}>
+                    <h1>
+                        <Body2023 text={"I am Andrey and i a programmer!"}/>
+                    </h1>
+                    <div className={styles.card}>
+                        <h3>MAP</h3>
+                        <div className={styles.content}>
+                            <NewComponent2023 students={students} cars={topCars}/>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <h3>Button</h3>
+                        <div className={styles.content}>
+                            {/*<button onClick={(event) => {console.log("Hello")}}>MyYouTubeChannel-1</button>*/}
+                            <button onClick={(event) => onClickHandler("Andrey")}>MyYouTubeChannel-1</button>
+                            <button onClick={(event) => onClickHandler("Olga")}>MyYouTubeChannel-2</button>
+                            {/*   <button onClick={function1}>1</button>
             <button onClick={(event) => function2(100200)}>2</button>*/}
-            <Button2023 name={'MyYouTubeChannel-1'} callback={() => ButtonOne('Andrey', 32)}/>
-            <Button2023 name={'MyYouTubeChannel-2'} callback={() => ButtonTwo('Olga', 34)}/>
-            <Button2023 name={'I am a stupid button'} callback={ButtonThree}/>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <h3>Universal Button with UseState</h3>
+                            <Button2023 name={'MyYouTubeChannel-1'} callback={() => ButtonOne('Andrey', 32)}/>
+                            <Button2023 name={'MyYouTubeChannel-2'} callback={() => ButtonTwo('Olga', 34)}/>
+                            <Button2023 name={'I am a stupid button'} callback={ButtonThree}/>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <h3>Universal Button with UseState</h3>
 
-          <div className={styles.content}>
-            <h2>{a}</h2>
-            <Button2023 name={'PlusOne'} callback={plusOne}/>
-            <Button2023 name={'0'} callback={zero}/>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <h3>Filter</h3>
-          {/*  <ul>
+                        <div className={styles.content}>
+                            <h2>{a}</h2>
+                            <Button2023 name={'PlusOne'} callback={plusOne}/>
+                            <Button2023 name={'0'} callback={zero}/>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <h3>Filter</h3>
+                        {/*  <ul>
               {currentMoney.map((banknotes, index) =>
                 <li key={index}>
                   <span style={{paddingLeft: '10px'}}>{banknotes.banknotes}</span>
@@ -162,47 +163,50 @@ const App2023 = () => {
               <Button2023 name={'Rubles'} callback={() => OnClickFilterHandler('Rubles')}/>
               <Button2023 name={'All'} callback={() => OnClickFilterHandler('All')}/>
             </div>*/}
-          <div className={styles.content}>
-            <ComponentFilter2023
-              currentMoney={currentMoney}
-              OnClickFilterHandler={OnClickFilterHandler}
-            />
-          </div>
-        </div>
-      </section>
-      <footer className={styles.footer}>
-        <ul>
-          <li>
-            <a href="https://it-incubator.io/" target='_blank'>
-              <img src={HomeIcon} alt=""/>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="https://it-incubator.io/education/front-end" target='_blank'>
-              <img src={ComputerIcon} alt=""/>
-              All Courses
-            </a>
-          </li>
-          <li>
-            <a href="https://sandbox.it-incubator.io/" target='_blank'>
-              <img src={SandBox} alt=""/>
-              Sandbox
-            </a>
-          </li>
-          <li>
-            <a href="https://it-incubator.io/contacts" target='_blank'>
-              <img src={Contacts} alt=""/>
-              Contacts
-            </a>
-          </li>
-        </ul>
-        <Footer2023 text={2023}/>
-      </footer>
-      </body>
-    </>
+                        <div className={styles.content}>
+                            <ComponentFilter2023
+                                currentMoney={currentMoney}
+                                OnClickFilterHandler={OnClickFilterHandler}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <footer>
+                <div className={styles.container}>
+                    <ul className={styles.footer}>
+                        <li>
+                            <a href="https://it-incubator.io/" target='_blank'>
+                                <img src={HomeIcon} alt=""/>
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://it-incubator.io/education/front-end" target='_blank'>
+                                <img src={ComputerIcon} alt=""/>
+                                All Courses
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://sandbox.it-incubator.io/" target='_blank'>
+                                <img src={SandBox} alt=""/>
+                                Sandbox
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://it-incubator.io/contacts" target='_blank'>
+                                <img src={Contacts} alt=""/>
+                                Contacts
+                            </a>
+                        </li>
+                    </ul>
+                    <Footer2023 text={2023}/>
+                </div>
+            </footer>
+            </body>
+        </>
 
-  );
+    );
 };
 
 export default App2023;
